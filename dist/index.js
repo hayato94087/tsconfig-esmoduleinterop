6973c1d816b46d9bfc4df486ed316643e08eada0
hayato94087 <hayato94087@gmail.com>
2024-03-24 06:36:45 +0900

    feat:esModuleInteropを有効化しエラーを解消

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,9 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const snakecase_keys_1 = require("snakecase-keys");
+const snakecase_keys_1 = __importDefault(require("snakecase-keys"));
 const user = { userName: 'micheal', emailAddress: 'taro@gmail.com' };
 const userSnakeCase = (0, snakecase_keys_1.default)(user);
 console.log(userSnakeCase);
